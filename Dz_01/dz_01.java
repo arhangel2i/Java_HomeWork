6d16cc290b2a70ed6c1f07ad25f40d857e00c79d
arhangel2i <arhangel2i@mail.ru>
2022-06-19 21:36:31 +0300

    dz_01 fix problem № 1

--- Dz_01/dz_01.java
+++ Dz_01/dz_01.java
@@ -11,31 +11,12 @@ public class dz_01 {
         System.out.println("Пример 3: а = 3, b = 0, ответ: " + sqrt(3, 0));
         System.out.println("Пример 4: а = 0, b = 0, ответ: " + sqrt(0, 0));
 
-        double a=0;double b = 0;
-        System.out.println("Пример 5: Cчитывание данных из файла");
-        try(BufferedReader br = new BufferedReader(new FileReader("input.txt"))){
-            String str;
-            while((str = br.readLine())!=null)
-            {
-                System.out.println(str);
-                String fChar = str.substring(0,1);
-                switch (fChar) {
-                    case "a":
-                        a=Double.parseDouble(str.substring(2));
-                        break;
-                    case "b":
-                        b=Double.parseDouble(str.substring(2));
-                        break;
-                }
-            }
-            br.close();
-        }
-        catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        String retRes = sqrt(a, b);
-        System.out.println("Запись вычислений в файл");
+        System.out.println("Пример 5: Cчитывание данных из файла");     
+        double[] rResult = ReadResult("input.txt");
+        String retRes = sqrt(rResult[0], rResult[1]);
+        DecimalFormat dF = new DecimalFormat("#.######");
+        System.out.printf("a=%s b=%s\n", dF.format(rResult[0]),dF.format(rResult[1]));  
+        System.out.println("Пример 5: Запись вычислений в файл");
         System.out.println(retRes);
         WriteResult(retRes, "output.txt");
     }
@@ -45,6 +26,31 @@ public class dz_01 {
         String retRes = a==0?"не определено":dF.format(Math.pow(a, b));;
         return retRes;
     }
+
+    public static double[] ReadResult(String FileName){
+        double[] retVal = new double[] {0,0};
+        try(BufferedReader br = new BufferedReader(new FileReader(FileName))){
+            String str;
+            while((str = br.readLine())!=null)
+            {
+                String fChar = str.substring(0,1);
+                switch (fChar) {
+                    case "a":
+                        retVal[0]=Double.parseDouble(str.substring(2));
+                        break;
+                    case "b":
+                        retVal[1]=Double.parseDouble(str.substring(2));
+                        break;
+                }
+            }
+            br.close();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return retVal;
+    }
+
     public static void WriteResult(String retRes, String FileName)
     {
         try(FileWriter fw = new FileWriter(FileName, false)){
